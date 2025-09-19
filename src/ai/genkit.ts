5d95e2ed946d7a7db147ac6b106ebc64363363c8
AAKSHANT KUMAR <kumaraakshant2005@gmail.com>
2025-09-19 21:44:52 +0000

    Well lets deploy it on vercel

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,9 @@
-import {genkit} from 'genkit';
+import {genkit, getModel} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {isProduction} from 'genkit/env';
 
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
+  enableTracing: !isProduction(),
 });
